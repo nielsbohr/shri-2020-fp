9f59fd931220268ee79564d796de8e034d8fa1ec
nielsbohr <root@DESKTOP-3QCMO93.localdomain>
2020-04-11 20:39:38 +0300

    done

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -1,39 +1,12 @@
-/**
- * @file Домашка по FP ч. 2
- * 
- * Подсказки:
- * Метод get у инстанса Api – каррированый
- * GET / https://animals.tech/{id}
- * 
- * GET / https://api.tech/numbers/base
- * params:
- * – number [Int] – число
- * – from [Int] – из какой системы счисления
- * – to [Int] – в какую систему счисления
- * 
- * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
- * Ответ будет приходить в поле {result}
- */
 import * as R from 'ramda';
-import Api from '../tools/api';
+import Api from '../tools/api'; 
 
 const api = new Api();
 
-/**
- * Я – пример, удали меня
- */
-const wait = time => new Promise(resolve => {
-    setTimeout(resolve, time);
-})
-
 const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-    /**
-     * Я – пример, удали меня
-     */
-    writeLog(value);
 
-    if (
-        R.equals(false,
+    const validation = (val) => {
+        if (
             R.allPass([
                 R.compose(
                     R.gt(R.__, 2),
@@ -46,31 +19,55 @@ const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
                 R.compose(
                     R.gt(R.__, 0),
                     R.length,
-                    R.match(/^[0-9]*$/g, R.__)
+                    R.match(/^[0-9]*\.?[0-9]*$/g, R.__)
                 ),
-            ])(value)
-        )
-    ) handleError();
+            ])(val)
+        ) {
+            return val;
+        } else handleError();
+    }
 
-    writeLog(parseInt(value));
+    const writeLogwithReturningValue = (val) => {
+        writeLog(val);
+        return val;
+    }
 
-    api.get('https://api.tech/numbers/base', {from: 10, to: 2, number: value}).then(({result}) => {
-        writeLog(result);
-        writeLog(result.length);
-        writeLog(result ** 2);
-    });
+    const secondStep = (value) => {
+        try {
+            (async() => {
+                const { result } = await R.compose(
+                    (id) => api.get(`https://animals.tech/${id}`)(id),
+                    writeLogwithReturningValue,
+                    (val) => val % 3,
+                    writeLogwithReturningValue,
+                    (val) => val ** 2,
+                    writeLogwithReturningValue,
+                    R.length,
+                    writeLogwithReturningValue
+                )(value);
 
-    wait(2500).then(() => {
-        writeLog('SecondLog')
+                handleSuccess(result);
+            })();
+        } catch(e) {
+            handleError(e);
+        }
+    }
 
-        return wait(1500);
-    }).then(() => {
-        writeLog('ThirdLog');
+    try {
+        (async () => {
+            const { result } = await R.compose(
+                (val) => api.get('https://api.tech/numbers/base', {from: 10, to: 2, number: val}),
+                writeLogwithReturningValue,
+                (val) => Math.round(val),
+                validation,
+                writeLogwithReturningValue,
+            )(value);
 
-        return wait(400);
-    }).then(() => {
-        handleSuccess('Done');
-    });
+            secondStep(result);
+        })();
+    } catch(e) {
+        handleError(e);
+    }
 }
 
 export default processSequence;
